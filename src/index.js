237e443b27388d87d86d95e9d65feba0527a7d1b
chenwenhua <wenhua.chen@versa-ai.com>
2018-08-05 12:23:22 +0800

    fix 接口请求

--- src/index.js
+++ src/index.js
@@ -10,11 +10,12 @@ const api = require('./router/api');
 const main = require('./router/main');
 
 //父级路由
-router.use('/',main.routes(),main.allowedMethods());
-router.use('/api',api.routes(),api.allowedMethods());
+router.use('/', main.routes(), main.allowedMethods());
+router.use('/api', api.routes(), api.allowedMethods());
 
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000, () => {
   console.log('app listen on 3000')
--- src/router/api.js
+++ src/router/api.js
@@ -1,18 +1,30 @@
 // api 路由
 const Router = require('koa-router');
+const axios = require('axios');
 
 //子路由1
 let api = new Router()
 
-api.get('/demo', async(ctx)=>{
+api.get('/demo', async (ctx)=>{
   ctx.body = {
     code: 0,
     result: 'demo'
   }
 })
 
-api.get('/todo',async(ctx)=>{
-  ctx.body = "api/todo";
+api.get('/mtl',async (ctx)=>{  
+  const url = 'https://m.moretickets.com/showapi/pub/site/1001/banner/app?siteCityOID=3101&time=1533441491946&src=m_web'
+  try {
+    const res = await axios.get(url)
+    ctx.body = res.data.result 
+  } catch (err) {
+    // console.log(err)
+    // console.log(err.response)
+    ctx.body = {
+      success: false,
+      message: err.response.data.message
+    }
+  }   
 })
 
 module.exports = api
